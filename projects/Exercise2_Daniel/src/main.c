4a3190496a38572ef63eae5228f69e13a272a41e
Daniel Benyei <benyeid@gmail.com>
2019-09-24 15:26:56 +0200

    Moved second declarations to comment

---
+++ projects/Exercise2_Daniel/src/main.c
@@ -0,0 +1,26 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
+
+**********************************************************************/
+#include "stm32f30x_conf.h"
+#include "30021_io.h" // Input/output library for this course
+
+int main(void)
+{
+    init_usb_uart( 9600 ); // Initialize USB serial emulation at 9600 baud
+    printf("Hello World!\n"); // Show the world you are alive!
+    while(1)
+    {
+
+
+    }
+}

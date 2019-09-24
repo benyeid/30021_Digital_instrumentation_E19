6e0b10017136487627ddbbcdab57791f6a5d4ba3
Daniel Benyei <benyeid@gmail.com>
2019-09-24 15:45:20 +0200

    Added basic, working LCD init function

--- projects/Exercise2_Daniel/src/main.c
+++ projects/Exercise2_Daniel/src/main.c
@@ -14,13 +14,27 @@
 #include "stm32f30x_conf.h"
 #include "30021_io.h" // Input/output library for this course
 
+#include "flash.h"
+#include "lcd.h"
+//#include "charset.h"
+
 int main(void)
 {
     init_usb_uart( 9600 ); // Initialize USB serial emulation at 9600 baud
     printf("Hello World!\n"); // Show the world you are alive!
+
+    int a=120;
+    uint8_t fbuffer[512];
+
+    // lcd_reset();
+
+    init_spi_lcd();
+    memset(fbuffer,0xAA,512);
     while(1)
     {
-
-
+        lcd_push_buffer(fbuffer);
+        printf("a = %d ", a);
     }
+
+
 }
